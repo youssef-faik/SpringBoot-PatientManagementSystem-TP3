208327c255a3dd975fa31ac5e658570428704101
Youssef Faik <faik.yusef@gmail.com>
2025-03-17 09:31:17 +0000

    Initial Commit

---
+++ src/main/java/org/example/tp3spring/Tp3SpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.tp3spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp3SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tp3spring/Tp3SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tp3spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp3SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

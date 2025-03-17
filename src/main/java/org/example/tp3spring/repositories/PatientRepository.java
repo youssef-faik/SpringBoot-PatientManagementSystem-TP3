4cbbaa006975253791d88b55b4d67ec94a85930e
Youssef Faik <faik.yusef@gmail.com>
2025-03-17 10:25:17 +0000

    Implement pagination and keyword search in PatientRepository and update PatientController

--- src/main/java/org/example/tp3spring/repositories/PatientRepository.java
+++ src/main/java/org/example/tp3spring/repositories/PatientRepository.java
@@ -2,7 +2,8 @@ package org.example.tp3spring.repositories;
 
 import org.example.tp3spring.entities.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
-    Patient findByNom(String nom);
-}
+    Page<Patient> findByNomContainsIgnoreCase(String keyword, Pageable pageable);}
--- src/main/java/org/example/tp3spring/web/PatientController.java
+++ src/main/java/org/example/tp3spring/web/PatientController.java
@@ -4,12 +4,16 @@ import lombok.RequiredArgsConstructor;
 import org.example.tp3spring.entities.Patient;
 import org.example.tp3spring.repositories.PatientRepository;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/patients")
 @RequiredArgsConstructor
+@Controller
 public class PatientController {
     private final PatientRepository patientRepository;
 
@@ -26,5 +30,24 @@ public class PatientController {
     @PostMapping
     public Patient savePatient(@RequestBody Patient patient) {
         return patientRepository.save(patient);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/index";
+    }
+
+    @GetMapping("/index")
+    public String index(Model model,
+                        @RequestParam(name = "page", defaultValue = "0") int page,
+                        @RequestParam(name = "size", defaultValue = "5") int size,
+                        @RequestParam(name = "keyword", defaultValue = "") String keyword) {
+        Page<Patient> pagePatients = patientRepository.findByNomContainsIgnoreCase(keyword, PageRequest.of(page, size));
+        model.addAttribute("listPatients", pagePatients.getContent());
+        model.addAttribute("pages", new int[pagePatients.getTotalPages()]);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", pagePatients.getTotalPages());
+        model.addAttribute("keyword", keyword);
+        return "patients";
     }
 }
